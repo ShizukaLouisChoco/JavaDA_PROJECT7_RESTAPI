7d05ee7261075f5a717bec662538c7d9451dc1b3
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-03-27 00:18:35 +0200

    Correction of HomeController class - add login page

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.nnk.springboot.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class HomeController
 {
@@ -19,5 +21,9 @@ public class HomeController
 		return "redirect:/bidList/list";
 	}
 
+	@RequestMapping("/login")
+	public String login(){
+		return "login";
+	}
 
 }
