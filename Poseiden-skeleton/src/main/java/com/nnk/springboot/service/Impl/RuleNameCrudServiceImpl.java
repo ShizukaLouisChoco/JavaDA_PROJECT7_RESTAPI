8f336ddf2bf4f14b66e987600e7d3b90a3d83eb7
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-04-28 00:09:42 +0200

    Create a Rating Entity

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/Impl/RuleNameCrudServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/Impl/RuleNameCrudServiceImpl.java
@@ -4,13 +4,11 @@ import com.nnk.springboot.domain.RuleName;
 import com.nnk.springboot.repositories.RuleNameRepository;
 import com.nnk.springboot.service.AbstractCrudService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Slf4j
-@Service
-@Qualifier("RuleNameCrudServiceImpl")
-public class RuleNameCrudServiceImpl extends AbstractCrudService<RuleName> {
+@Service("RuleNameCrudServiceImpl")
+public class RuleNameCrudServiceImpl extends AbstractCrudService<RuleName,RuleNameRepository> {
 
     public RuleNameCrudServiceImpl(RuleNameRepository repository){
         super(repository);
