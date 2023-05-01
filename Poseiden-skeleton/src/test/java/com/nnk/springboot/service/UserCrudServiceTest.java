d332a904b12c724561bf8c6a06e218ad364fc893
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-05-01 18:01:05 +0200

    hashing of the password

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/service/UserCrudServiceTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/service/UserCrudServiceTest.java
@@ -9,27 +9,51 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 public class UserCrudServiceTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    PasswordEncoder passwordEncoder;
+
     @InjectMocks
     UserCrudServiceImpl userCrudService;
 
     @Test
     @DisplayName("tradeCrudService calls tradeRepository create method")
-    public void given_whenCreateTrade_thenReturnTrade() {
+    public void given_whenCreateUser_thenReturnUser() {
         //GIVEN
-        User user = new User("fullname","username","role","password");
+        User user = new User("fullname","username3","USER","password");
         //WHEN
         userCrudService.create(user);
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("$2a$10$Nf4NPdHPIB2vfdt7WBb4/OrNj5BSHq.S6yfE7IuUmVjGzeFY.v/xG ");
+        //THEN
+        ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userRepository, times(1)).save(userArgumentCaptor.capture());
+
+        assertThat(userArgumentCaptor.getValue())
+                .isNotNull()
+                .satisfies(arg -> {
+                    assertThat(arg.getId()).isEqualTo(user.getId());
+                });
+
+    }
+
+    @Test
+    @DisplayName("tradeCrudService calls tradeRepository create method")
+    public void given_whenCreateUserAdmin_thenReturnUserAdmin() {
+        //GIVEN
+        User user = new User("fullname","username3","ADMIN","password");
+        //WHEN
+        userCrudService.create(user);
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("$2a$10$Nf4NPdHPIB2vfdt7WBb4/OrNj5BSHq.S6yfE7IuUmVjGzeFY.v/xG ");
         //THEN
         ArgumentCaptor<User> userArgumentCaptor = ArgumentCaptor.forClass(User.class);
         verify(userRepository, times(1)).save(userArgumentCaptor.capture());
