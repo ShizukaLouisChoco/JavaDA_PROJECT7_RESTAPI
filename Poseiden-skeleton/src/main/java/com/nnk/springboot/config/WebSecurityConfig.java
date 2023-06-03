6b02ac26fd8dc1b9b2e562dd1a52028fef24e27a
ShizukaLouisChoco <chocokkuma@gmail.com>
2023-06-03 10:58:20 +0200

    Add css to permitAll for page layout before login

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/WebSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/user/*").permitAll()
+                .antMatchers("/css/**","/user/*").permitAll()
                 .antMatchers("/admin*").hasRole("ADMIN")
                 .anyRequest().fullyAuthenticated()
                 .and()
